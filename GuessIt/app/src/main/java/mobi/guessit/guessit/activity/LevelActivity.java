4ee940f3159fa3e49fcbcbc39ab0ea6d31825fe7
Marlon Andrade <marlonmandrade@gmail.com>
2014-07-26 12:45:57 -0300

    setting level activity background color

--- GuessIt/app/src/main/java/mobi/guessit/guessit/activity/LevelActivity.java
+++ GuessIt/app/src/main/java/mobi/guessit/guessit/activity/LevelActivity.java
@@ -1,10 +1,16 @@
 package mobi.guessit.guessit.activity;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
 import mobi.guessit.guessit.R;
+import mobi.guessit.guessit.model.Configuration;
+import mobi.guessit.guessit.model.Game;
+import mobi.guessit.guessit.model.UserInterfaceElement;
 
 public class LevelActivity extends Activity {
 
@@ -12,23 +18,32 @@ public class LevelActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
+
+        initializeView();
     }
 
+    private void initializeView() {
+        Game game = Configuration.getInstance().getGame();
+
+        UserInterfaceElement main = game.getUserInterface().getMain();
+
+        View background = findViewById(R.id.level_background);
+        background.setBackgroundColor(Color.parseColor(main.getBackgroundColor()));
+
+        // adjust keyboard color
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.level, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            // show settings activity
             return true;
         }
         return super.onOptionsItemSelected(item);
