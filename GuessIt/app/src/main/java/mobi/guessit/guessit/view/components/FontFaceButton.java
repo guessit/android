3cb320260fcd2dada6d7fed54fcf1f5545b71937
Marlon Andrade <marlonmandrade@gmail.com>
2014-08-03 11:07:57 -0300

    adding a button with custom font
    - implemented just like FontFaceTextView

---
+++ GuessIt/app/src/main/java/mobi/guessit/guessit/view/components/FontFaceButton.java
@@ -0,0 +1,42 @@
+package mobi.guessit.guessit.view.components;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.TextView;
+
+import mobi.guessit.guessit.R;
+
+public class FontFaceButton extends Button {
+
+    public FontFaceButton(Context context) {
+        super(context);
+        initializeView(null);
+    }
+
+    public FontFaceButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initializeView(attrs);
+    }
+
+    public FontFaceButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initializeView(attrs);
+    }
+
+    private void initializeView(AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray array = getContext().obtainStyledAttributes(attrs,
+                    R.styleable.FontFaceTextView);
+
+            String fontFace = array.getString(R.styleable.FontFaceTextView_fontFace);
+            int fontStyle = array.getInteger(R.styleable.FontFaceTextView_fontStyle, Typeface.NORMAL);
+
+            if (fontFace != null) {
+                setTypeface(Typeface.createFromAsset(getContext().getAssets(), "fonts/" + fontFace), fontStyle);
+            }
+        }
+    }
+}
