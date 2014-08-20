68cc8ccbabe6b6ac59d73193b05adebf9ab7c2fd
Marlon Andrade <marlonmandrade@gmail.com>
2014-08-20 18:35:39 -0300

    removing unused imports

--- GuessIt/app/src/main/java/mobi/guessit/guessit/activity/LevelActivity.java
+++ GuessIt/app/src/main/java/mobi/guessit/guessit/activity/LevelActivity.java
@@ -6,12 +6,10 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import mobi.guessit.guessit.R;
 import mobi.guessit.guessit.helper.ColorHelper;
